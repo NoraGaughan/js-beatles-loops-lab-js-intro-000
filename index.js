5571f31513d4ff5e96c594b50701dbee102aea3a
Nora Gaughan <noragaughan@gmail.com>
2020-07-14 16:26:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts() {
-  const facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice'];
+function johnLennonFacts(facts) {
   var array = [];
-  var i = 0;
-  while (facts.length > i) {
-    array.push(facts[i] + "!!!");
+  let i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
     i++;
   }
-  return array;
+  return array
 }
 
 function iLoveTheBeatles(number) {
